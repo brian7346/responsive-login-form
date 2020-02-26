cda7c59f160acd683c085c2309babeb9c2db40b6
brian7346 <brian2009@yandex.ru>
2020-02-26 20:55:45 +0400

    feat: styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 const gulp = require("gulp");
 const stylus = require("gulp-stylus");
+const browserSync = require("browser-sync");
+
 function css() {
-  gulp
+  return gulp
     .src(["styles/*.styl"])
     .pipe(stylus())
-    .pipe(gulp.dest("styles/"));
+    .pipe(gulp.dest("styles/"))
+    .pipe(browserSync.stream());
 }
 
 exports.default = function() {
